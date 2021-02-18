9e674d298693134d86ee09935d7899c441dcad1e
adumbidiot <nathaniel.daniel23@outlook.com>
2021-02-17 18:31:00 -0800

    Fix wasm

--- src/wasm_bindgen_support.rs
+++ src/wasm_bindgen_support.rs
@@ -8,13 +8,13 @@ pub fn compress(data: &str) -> Result<JsString, JsValue> {
 
     let compressed = crate::compress(&data);
 
-    JsString::from_code_point(&compressed)
+    Ok(JsString::from_char_code(&compressed))
 }
 
 #[wasm_bindgen]
 pub fn decompress(data: JsString) -> JsValue {
     // Returning a String crashes?
-    let data: Vec<u32> = data.iter().map(u32::from).collect();
+    let data: Vec<u16> = data.iter().collect();
     crate::decompress(&data)
         .map(JsString::from)
         .map(Into::into)
