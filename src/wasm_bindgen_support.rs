d628f594035d0e22478be7a7a68fb19d9979736e
adumbidiot <nathaniel.daniel23@outlook.com>
2021-02-18 22:01:06 -0800

    Make wasm bindings lossless

--- src/wasm_bindgen_support.rs
+++ src/wasm_bindgen_support.rs
@@ -1,20 +1,24 @@
 use js_sys::JsString;
-use wasm_bindgen::prelude::*;
+use wasm_bindgen::{
+    prelude::*,
+    JsCast,
+};
 
 /// Compress a [`JsString`].
 ///
 #[wasm_bindgen]
-pub fn compress(data: JsString) -> Result<JsString, JsValue> {
+pub fn compress(data: &JsValue) -> JsString {
+    let data: &JsString = data.dyn_ref::<JsString>().expect("Valid JsString");
     let data: Vec<u16> = data.iter().collect();
     let compressed = crate::compress(&data);
-    Ok(JsString::from_char_code(&compressed))
+    JsString::from_char_code(&compressed)
 }
 
 /// Decompress a [`JsString`].
 ///
 #[wasm_bindgen]
-pub fn decompress(data: JsString) -> JsValue {
-    // Returning a String crashes?
+pub fn decompress(data: &JsValue) -> JsValue {
+    let data: &JsString = data.dyn_ref::<JsString>().expect("Valid JsString");
     let data: Vec<u16> = data.iter().collect();
     crate::decompress(&data)
         .map(|s| JsString::from_char_code(&s))
