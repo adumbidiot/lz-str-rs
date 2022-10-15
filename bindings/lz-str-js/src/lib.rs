47561dbe3b9e698d5d238dd0cbcc7e3c88fc42cd
adumbidiot <nathaniel.daniel23@outlook.com>
2022-10-14 20:14:51 -0700

    Move wasm binding to separate crate

--- bindings/lz-str-js/src/lib.rs
+++ bindings/lz-str-js/src/lib.rs
@@ -1,23 +1,22 @@
 use js_sys::JsString;
-use wasm_bindgen::{prelude::*, JsCast};
+use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
 
 /// Compress a [`JsString`].
-///
 #[wasm_bindgen]
 pub fn compress(data: &JsValue) -> JsString {
-    let data: &JsString = data.dyn_ref::<JsString>().expect("Valid JsString");
+    let data: &JsString = data.dyn_ref::<JsString>().expect("invalid `JsString`");
     let data: Vec<u16> = data.iter().collect();
-    let compressed = crate::compress(&data);
+    let compressed = lz_str::compress(&data);
     JsString::from_char_code(&compressed)
 }
 
 /// Decompress a [`JsString`].
-///
 #[wasm_bindgen]
 pub fn decompress(data: &JsValue) -> JsValue {
-    let data: &JsString = data.dyn_ref::<JsString>().expect("Valid JsString");
+    let data: &JsString = data.dyn_ref::<JsString>().expect("invalid `JsString`");
     let data: Vec<u16> = data.iter().collect();
-    crate::decompress(&data)
+    lz_str::decompress(&data)
         .map(|s| JsString::from_char_code(&s))
         .map(Into::into)
         .unwrap_or(JsValue::NULL)
