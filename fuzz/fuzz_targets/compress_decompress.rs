4eb08c5ad7ab07152fd28f7e64cfbdb0bfe66fb5
Nathaniel Daniel <nathaniel.daniel23@outlook.com>
2022-10-25 12:06:49 -0700

    Add basic fuzzing harness  (#35)
    * Add inital fuzz harness
    
    * Fix fuzz target
    
    * Change input type to one that impls arbitrary
    
    * Add fuzz lockfile

---
+++ fuzz/fuzz_targets/compress_decompress.rs
@@ -0,0 +1,8 @@
+#![no_main]
+use libfuzzer_sys::fuzz_target;
+
+fuzz_target!(|input: Vec<u16>| {
+     let compressed = lz_str::compress(&input);
+     let decompressed = lz_str::decompress(&compressed).unwrap();
+     assert!(input == decompressed);
+});
