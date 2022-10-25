04790627c07ae52eb95faeeb84e2f2dfcd33a11f
adumbidiot <nathaniel.daniel23@outlook.com>
2022-10-25 11:43:56 -0700

    Fix fuzz target

--- fuzz/fuzz_targets/compress_decompress.rs
+++ fuzz/fuzz_targets/compress_decompress.rs
@@ -1,8 +1,8 @@
 #![no_main]
 use libfuzzer_sys::fuzz_target;
 
-fuzz_target!(|input: &[u8]| {
-     let compressed = lz_str::compress(&data);
+fuzz_target!(|input: &[u16]| {
+     let compressed = lz_str::compress(input);
      let decompressed = lz_str::decompress(&compressed).unwrap();
      assert!(input == decompressed);
 });
