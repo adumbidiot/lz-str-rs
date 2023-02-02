3aea8d6590fae4b88b2e287208f7271c229a257f
Nathaniel Daniel <nathaniel.daniel23@outlook.com>
2023-02-02 04:13:14 -0800

    Fix clippy (#43)
    * Fix clippy
    
    * Update valid_inputs.rs

--- tests/invalid_inputs.rs
+++ tests/invalid_inputs.rs
@@ -5,7 +5,7 @@ fn invalid_decompress() {
     let invalid_data = &["bed123", "zed123", "ed[[[[d1d[[[[dF9]"];
 
     for data in invalid_data {
-        eprintln!("Decompressing '{}'", data);
+        eprintln!("Decompressing '{data}'");
         let arr: Vec<u16> = data.encode_utf16().collect();
         assert!(decompress(arr).is_none());
     }
